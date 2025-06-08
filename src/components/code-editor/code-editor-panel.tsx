6a813476f56637b2a192f982b027961b6b150f8d
AMrit DAsh <amrit.dash60@gmail.com>
2025-06-08 20:28:45 +0000

    The height issue is still not fixed.

--- src/components/code-editor/code-editor-panel.tsx
+++ src/components/code-editor/code-editor-panel.tsx
@@ -158,9 +158,9 @@ export function CodeEditorPanel() {
           {activeFilePath && openedFiles.has(activeFilePath) && (
              <TabsContent 
                 value={activeFilePath} 
-                className="flex-1 flex flex-col p-0 m-0 overflow-hidden min-h-0"
+                className="flex-1 p-0 m-0 overflow-auto min-h-0"
               > 
-                <ScrollArea className="flex-1 w-full min-h-0">
+                <ScrollArea className="h-full w-full">
                   <Textarea
                     value={currentContent}
                     onChange={handleContentChange}
